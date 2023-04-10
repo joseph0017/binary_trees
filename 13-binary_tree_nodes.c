6703ea8c85686b62457f8d9e8d2e7caa0d35a972
joseph0017 <josephogiku96@gmail.com>
2023-04-10 10:35:19 -0700

    Binary Tress

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - function that counts the nodes
+ * with at least 1 child in a binary tree
+ * @tree: is a pointer to the root node of the tree
+ * Return: return 0, if tree is NULL
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree && tree->left)
+		return (0);
+
+	if (!tree && tree->right)
+		return (0);
+
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
