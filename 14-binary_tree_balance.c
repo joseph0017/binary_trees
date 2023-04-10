8b6b67a722b94e01b711b384708ca6fea60c7204
joseph0017 <josephogiku96@gmail.com>
2023-04-10 11:01:29 -0700

    Binary Tress

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree:  is a pointer to the root node of the tree to measure the height
+ * Return:  0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
+ * binary_tree_balance - function that measures the balance
+ * factor of a binary tree
+ * @tree: is a pointer to the root node of the tree
+ * Return: return 0, if tre is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	balance = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+	return (balance);
+}
