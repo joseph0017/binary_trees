88b870857ccc83d7e600081c84e6af7252da6fd2
joseph0017 <josephogiku96@gmail.com>
2023-04-10 10:48:55 -0700

    Binary Tress

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,14 +8,11 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL || tree->left == NULL)
 		return (0);
 
-	if (!tree && !tree->left)
-		return (0);
-
-	if (!tree && !tree->right)
-		return (0);
+	if (tree == NULL || tree->right == NULL)
+                return (0);
 
 	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
