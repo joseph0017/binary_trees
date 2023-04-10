5492f096f41bafb48004195850f894b1ec1eaa59
joseph0017 <josephogiku96@gmail.com>
2023-04-10 10:21:49 -0700

    Binary Tress

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: is a pointer to the root node of the tree
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL)
+		return (1);
+
+	if (tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
+}
