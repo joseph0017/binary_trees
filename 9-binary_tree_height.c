0a7ee4b1101033bfabfce2d6278271fc6c068803
joseph0017 <josephogiku96@gmail.com>
2023-04-10 08:57:12 -0700

    Binary Tress

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,9 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left_height += binary_tree_height(tree->left);
+		left_height = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		right_height += binary_tree_height(tree->right);
+		right_height = 1 + binary_tree_height(tree->right);
 
 	return (left_height > right_height ? left_height : right_height);
 }
